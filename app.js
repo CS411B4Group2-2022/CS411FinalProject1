4ba8babbba74fe703602bd9facb7dd08f653c1aa
Michael Walsh <mpw9@bu.edu>
2022-11-17 21:11:33 -0500

    it's so bad

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
 const express = require("express");
-const axios = require("axios");
+
+const axios = require('axios');
+
+const $ = require('jquery')
+
 const { appendFile } = require("fs");
-const {response} = require("express");
 
 const app = express();
+
+const cors = require('cors');
+const { response } = require("express");
+app.use(cors({
+  origin: '*'
+}));
 
 const API_URL = 'https://collectionapi.metmuseum.org/public/collection/v1/objects/';
 
 var total = 482855;
-
+/*
 function getRandomObject(max) {
-    return Math.floor(Math.random() * max);
+  return Math.floor(Math.random() * max);
 }
 
 var random_object = getRandomObject(total).toString();
@@ -18,24 +27,78 @@ var random_object = getRandomObject(total).toString();
 var object_data = API_URL.concat(random_object);
 
 axios.get(object_data)
-    .then(response => {
-        console.log(response.data);
-    })
+  .then(response => {
+      console.log(response.data);
+  })
 
 // make a variable for the isPublicDomain property
 var isPublicDomain = response.data.isPublicDomain;
 
 // if is publicDomain is false, then get a new random object
 while (isPublicDomain == false) {
-    getRandomObject(total);
+  getRandomObject(total);
 }
+*/
 
+/*
+function getRandomObject() {
+  var isPublicDomain = false;
+  while (isPublicDomain == false) {
+    var randomObjectID = (Math.floor(Math.random() * total)).toString;
+    var randomObjectURL = API_URL.concat(randomObjectID);
+    axios.get(randomObjectURL).then(function(response) {
+      objectData = response.data;
+    });
+    isPublicDomain = objectData.isPublicDomain;
+  };
+    imageURL = objectData.primaryImageSmall;
+    solution = objectData.country;
+};
+*/
 
 app.get("/", function (req, res) {
-  res.send({"image":"https://images.metmuseum.org/CRDImages/ad/web-large/DP263972.jpg", "solution":"United States"});
+  // Initalize globally useful variables
+  var imageURL = "https://i.scdn.co/image/ab67616d0000b2732d1f18d70f80bb99e75b0209";
+  var solution = "Morocco";
+  var objectData = JSON.parse('{"o":true}');
+  var isPublicDomain = false;
+  var helpme;
+
+  // While loop to find valid object
+  /*
+  while (isPublicDomain == false) {
+    var randomObjectID = (Math.floor(Math.random() * total)).toString; 
+    var randomObjectURL = API_URL.concat(randomObjectID);
+    /*
+    $.ajax({
+      type: "GET",
+      url: randomObjectURL,
+      datatype: "json",
+      success: function(response) {
+        objectData = response
+      }
+    })
+    
+    axios.get(randomObjectURL).then(function(response) {
+      objectData = JSON.parse(response);
+    });
+    isPublicDomain = objectData.isPublicDomain;
+  };
+  */
+  // Set Image URL and Solution to called values
+
+  axios.get('https://collectionapi.metmuseum.org/public/collection/v1/objects/234').then(resp => {
+    helpme = JSON.stringify(resp);
+    objectData = JSON.parse(resp);
+  });
+
+  imageURL = objectData.primaryImageSmall;
+  solution = objectData.country;
+
+  // Send response
+  res.send({"image":imageURL, "solution":solution, "objectData":helpme});
 });
 
 app.listen(3000, function () {
   console.log("Server is listening on localhost:3000");
 });
-
